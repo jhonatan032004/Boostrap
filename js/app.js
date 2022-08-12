3cbf8864c05d026278f71a0af48508f96889fe4c
jhonatan <jpalacio172@misena.edu.co>
2022-08-12 17:35:48 -0500

    segunda clase

--- js/app.js
+++ js/app.js
@@ -1,32 +1,46 @@
 let form = document.getElementById('form-product');
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     let lista = document.getElementById('list-product');
     let descripcion = document.getElementById('descripcion');
     let cantidad = document.getElementById('cantidad');
     let precio = document.getElementById('precio');
-
     let div = document.createElement('div');
-    div.innerHTML = `
-    <div class="card text-center ">
-    <div class="card-body cols-11">
-        <b>Descripcion: </b>${descripcion.value}
-        <b>Cantidad: </b>${cantidad.value}
-        <b>Precio: </b>${precio.value}
-        
-    </div>
-    <div class="col-1">
-        <a href="" 
-        class="btn btn-danger"
-        name="delete">
-        Borrar
-        </a>
-    </div>
-    
-</div>
-    
-    
+    div.classList.add('row');
+    div.classList.add('mb-1');
+    div.innerHTML = ` 
+    <div class="card text-center col-11""> 
+    <div class="card-body"> 
+    <b>Descripcion: </b>${descripcion.value} 
+    <b>Cantidad: </b>${cantidad.value} 
+    <b>Precio: </b>${precio.value} 
+    </div> 
+    </div> 
+    <button class="btn btn-danger text-center col-1" name="delete"> 
+    <i class="fa-solid fa-trash-can"></i> 
+    </button> 
     `;
-    lista.appendChild(div);
-
+    if (descripcion.value != "" && precio.value != "" && cantidad.value != "") {
+        lista.appendChild(div); mensaje('El producto ha sido agregado', 'success')
+        form.reset();
+    }
 })
+let borrar = document.querySelector("#list-product")
+borrar.addEventListener("click", function (e) {
+    if (e.target.name === "delete") {
+        e.target.parentElement.remove();
+        mensaje('El producto ha sido eliminado', 'danger')
+    }
+})
+const mensaje = function (msj, color) {
+    container = document.querySelector(".container");
+    app = document.querySelector("#app");
+    div = document.createElement("div");
+    div.innerHTML = ` 
+                    <div class="alert alert-${color} mt-5">${msj}</div> 
+                    `;
+    container.insertBefore(div, app);
+    setTimeout(() => {
+        div.remove();
+    }, 300);
+}
